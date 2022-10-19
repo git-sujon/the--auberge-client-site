579b1c87ed769c004996a9ed1e7a0fbbfbc19896
Mr-Robot-101 <git.sujon@gmail.com>
2022-10-19 14:11:55 +0600

    Firebase Config file add

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB62CQ1Dr6lc3QdNxNzbwDSrsOQduoFdjU",
+  authDomain: "the-auberge-9ab46.firebaseapp.com",
+  projectId: "the-auberge-9ab46",
+  storageBucket: "the-auberge-9ab46.appspot.com",
+  messagingSenderId: "1017703755619",
+  appId: "1:1017703755619:web:1170b568160a904c642d56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app; 
